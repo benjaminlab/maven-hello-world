f09f3836e802b362304a5e538d96f9222582272e
Benjamin Bach <benjamin.bach@labfolder.com>
2017-01-12 17:37:18 +0100

    add region

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,13 @@ docker push bbach/myrepo1:$SHA1
 EB_BUCKET=testbucketbenjamin
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+
+region=eu-central-1
+
+aws configure set aws_access_key_id $AWSKEY
+aws configure set aws_secret_access_key $AWSSECRETKEY
+aws configure set default.region $region
+
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name benjamin-test \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
